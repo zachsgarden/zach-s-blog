87adf8f985c2062548a59e6866fbe97a7b5a79e2
Zach Kamp <kamp.zach@gmail.com>
2022-10-19 00:06:47 -0400

    adding some more css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,36 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { container } from './layout.module.css'
+import * as React from "react";
+import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
-    return (
-        <div className={container}>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                </ul>
-            </nav>
-            <main>
-                <h1>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
-    )
-}
+  return (
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
